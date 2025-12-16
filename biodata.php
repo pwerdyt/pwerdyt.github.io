3e0d7c263121e178b1416287b88b46fed5863286
pwerdyt <pwerdyt@gmail.com>
2025-12-16 13:59:17 +0800

    first commit

---
+++ biodata.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>test bikin biodata</title><style>
+table, th, td {
+  border:1px solid black;
+}
+</style>
+</head>
+<body>
+    <table style="width:35%">
+        <tr>
+            <th colspan="3">BIODATA</th>
+        </tr>
+    <tr>
+        <td>Data diri</td>
+        <td>Keterangan</td>
+        <td>Foto</td>
+    </tr>
+    <tr>
+        <td>Nama</td>
+        <td>rizik</td>
+        <td rowspan="6"><img src="images.jpg" alt=""></td>
+    </tr>
+    <tr>
+        <td>Tempat tanggal lahir</td>
+        <td>maros, 18 maret 2000</td>
+    </tr>
+    <tr>
+        <td>Jenis kelamin</td>
+        <td>laki laki</td>
+    </tr>
+    <tr>
+        <td>Nim</td>
+        <td>20192105002</td>
+    </tr>
+    <tr>
+        <td>Kelas</td>
+        <td>S1.25</td>
+    </tr>
+    <tr>
+        <td>Jurusan</td>
+        <td>bsistem informasi</td>
+    </tr>
+   </table>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>web coba coba</title><style>
+table, th, td {
+  border:2px solid black;
+}
+</style>
+</head>
+<body><h2>jadwal pelajaran</h2>
+   <table>
+    <tr>
+        <td>senin</td>
+        <td>selasa</td>
+        <td>rabu</td>
+    </tr>
+    <tr>
+        <td>mtk</td>
+        <td>sosiologi</td>
+        <td>kimia</td>
+    </tr>
+    <tr>
+        <td>agama</td>
+        <td>fisika</td>
+        <td>biologi</td>
+    </tr>
+    <tr>
+        <td>bahasa indonesia</td>
+        <td>bahasa inggris</td>
+        <td>mtk</td>
+    </tr>
+   </table>
+   <img src="logo.jpg" alt="">
+</body>
+</html>
